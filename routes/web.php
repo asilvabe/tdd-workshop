d0abfeb1cac58a27d1a3a4ef74413b87f6644085
Alejandro Silva Berbesi <me@asilvabe.dev>
2023-04-09 11:26:36 -0500

    refactor: add post controller and rename index posts view

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/posts', 'posts');
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');

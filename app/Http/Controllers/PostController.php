3f1f1fa7a6f8c11962b40fab3d0ea8bb7683d944
Alejandro Silva Berbesi <me@asilvabe.dev>
2023-04-09 11:56:29 -0500

    feat: show the posts title and creation date

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\View\View;
 
 class PostController extends Controller
 {
     public function index(): View
     {
-        return view('posts.index');
+        return view('posts.index', [
+            'posts' => Post::all(),
+        ]);
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,1 +1,7 @@
 <h1>Posts list</h1>
+
+<ul>
+    @foreach($posts as $post)
+        <li>{{ $post->title }} - {{ $post->created_at->format('d/m/Y') }}</li>
+    @endforeach
+</ul>

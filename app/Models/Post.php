78680e5411beb9a17328866ffce6284d3d7aa5b5
Alejandro Silva Berbesi <me@asilvabe.dev>
2023-04-10 12:04:23 -0500

    refactor: add local scope for approved posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function scopeApproved(Builder $query): Builder
+    {
+        return $query->whereNotNull('approved_at');
+    }
 }

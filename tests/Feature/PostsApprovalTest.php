f554e98c4e95dce3d21b8b6f8c08885b0d0a4bf2
Alejandro Silva Berbesi <me@asilvabe.dev>
2023-04-10 19:06:10 -0500

    tests: an email is sent to the post author when the post is approved

--- tests/Feature/PostsApprovalTest.php
+++ tests/Feature/PostsApprovalTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Mail\PostApprovedMail;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
 
 class PostsApprovalTest extends TestCase
@@ -39,5 +41,26 @@ class PostsApprovalTest extends TestCase
             ->assertForbidden();
 
         $this->assertFalse($post->fresh()->isApproved());
+    }
+
+    /** @test */
+    public function an_email_is_sent_to_the_post_author_when_the_post_is_approved(): void
+    {
+        Mail::fake();
+
+        $post = Post::factory()->unapproved()->create();
+
+        $this->assertFalse($post->isApproved());
+
+        $this
+            ->actingAs(User::factory()->admin()->create())
+            ->put("/posts/{$post->id}/approve")
+            ->assertRedirect('/posts');
+
+        $this->assertTrue($post->fresh()->isApproved());
+
+        Mail::assertSent(PostApprovedMail::class, function(PostApprovedMail $mail) use ($post) {
+            return $mail->hasTo($post->author->email);
+        });
     }
 }
